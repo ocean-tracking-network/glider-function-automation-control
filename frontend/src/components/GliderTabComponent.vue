61b0ca3b5b7076629f9b85d716e703d5067773ad
Cole Baxter <w0516290@nscc.ca>
2026-02-19 13:42:40 -0400

    fix: glider-tab add/remove modal refinements
    Closes #22
    
    Signed-off-by: Cole Baxter <w0516290@nscc.ca>

--- frontend/src/components/GliderTabComponent.vue
+++ frontend/src/components/GliderTabComponent.vue
@@ -1,60 +1,83 @@
 <script setup>
+import { ref } from 'vue';
 import { useGlidersStore } from '@/stores/gliders';
 import { storeToRefs } from 'pinia';
 
 const gliderStore = useGlidersStore()
 const { gliders, selected_glider_idx } = storeToRefs(gliderStore)
 
+const new_glider_name = ref('')
+const selected_delete = ref({})
+const add_prompt = ref(false)
+const delete_prompt = ref(false)
+const error_msg = ref('')
+
 function enable_disable_glider() {
   gliderStore.enable_disable_selected_glider()
 }
 
-function add_glider_prompt() {
-  const rawName = prompt('Please enter new glider name', '')
-  if (rawName === null) {
-    return
-  }
-
-  const name = rawName.trim()
+function add_glider() {
+  const name = new_glider_name.value.trim()
 
   if (!name) {
-    alert('Invalid: Name is empty')
+    error_msg.value = 'Invalid: Name is empty'
     return
   }
   if (name.includes(' ')) {
-    alert('Invalid: Name cannot include spaces')
+    error_msg.value = 'Invalid: Name cannot include spaces'
     return
   }
   if (gliders.value.some((glider) => glider.name === name)) {
-    alert(`Invalid: "${name}" is already taken`)
-    return
-  }
-
-  if (!confirm(`Are you sure you want to create glider '${name}''`)) {
+    error_msg.value = `Invalid: "${name}" is already taken`
     return
   }
 
   gliderStore.save_glider(name)
-}
-function delete_glider_prompt(glider) {
-  if (confirm(`Are you sure you want to delete glider ${glider.name}`)) {
-    gliderStore.delete_glider(glider)
-  }
+  error_msg.value = ''
+  add_prompt.value = ''
+  new_glider_name.value = ''
 }
 </script>
 <template>
-  <div class="border">
+  <div class="border tab-border">
     <div id="button-div">
       <button v-for="(glider, index) in gliders" :class="{ selected: selected_glider_idx == index, gliders: true }"
         @click="gliderStore.select_glider(index)">
         <input v-model="glider.enabled" @change="enable_disable_glider" :disabled="selected_glider_idx != index"
           class="enable" type="checkbox">
         {{ glider.name }}
-        <button v-if="gliders[selected_glider_idx]._id == glider._id" @click="delete_glider_prompt(glider)" class="x-btn">X</button>
+        <button v-if="gliders[selected_glider_idx]._id == glider._id"
+          @click="selected_delete = glider; delete_prompt = true" class="x-btn">X</button>
       </button>
-      <button class="gliders" id="add" @click="add_glider_prompt()">+</button>
+      <button class="gliders" id="add" @click="add_prompt = true">+</button>
     </div>
-      </div>
+  </div>
+
+  <Teleport defer to="#main-flex">
+    <div v-if="add_prompt" class="border modal">
+        <h2 class="modal-header">Add a New Glider</h2>
+        <form action="">
+          <p v-if="error_msg" class="danger">{{ error_msg }}</p>
+          <input v-model="new_glider_name" type="text" placeholder="Enter Glider Name">
+          <br>
+          <button type="button" class="border" @click="add_glider()">Add</button>
+          <button type="button" class="border"
+            @click="error_msg = ''; new_glider_name = ''; add_prompt = false">Cancel</button>
+        </form>
+    </div>
+  </Teleport>
+
+  <Teleport defer to="#main-flex">
+    <div v-if="delete_prompt" class="border modal">
+        <h2 class="modal-header">Are You Sure You Want to Delete Glider: {{ selected_delete.name ?? delete_prompt == false}}</h2>
+        <form action="">
+          <button type="button" class="border danger"
+            @click="gliderStore.delete_glider(selected_delete); delete_prompt = false">Delete</button>
+          <button type="button" class="border"
+            @click="selected_delete = {}; delete_prompt = false">Cancel</button>
+        </form>
+    </div>
+  </Teleport>
 </template>
 <style scoped>
 #tab-body {
@@ -63,7 +86,7 @@ function delete_glider_prompt(glider) {
   border-width: 2px;
 }
 
-.border {
+.tab-border {
   border-bottom-width: 0;
   border-bottom-left-radius: 0;
   border-bottom-right-radius: 0;
@@ -120,4 +143,51 @@ function delete_glider_prompt(glider) {
 .enable {
   margin-right: .5rem;
 }
+
+.danger {
+  color: red;
+}
+
+.modal {
+  background-color: var(--color-background);
+  padding: 2rem;
+  text-align: center;
+  width: 300px;
+
+  position: fixed;
+  top: 30%;
+  left: 50%;
+  z-index: 1000;
+  transform: translate(-50%, -50%);
+}
+
+.modal-header {
+  margin-bottom: 2rem;
+  font-size: x-large;
+}
+
+form button {
+  transition: .3s;
+  /* float: left; */
+  /* margin: 1rem 0 0 2rem; */
+  width: 30%;
+  display: inline;
+  margin: 10%;
+}
+
+form button:hover {
+  transition: .3s;
+  background-color: var(--color-border-hover);
+}
+
+input {
+  background-color: var(--color-background-soft);
+  color: var(--color-text);
+  border-radius: 3px;
+  border-color: lightgray;
+  border-width: 1px;
+  padding: .2rem;
+}
+
+
 </style>
