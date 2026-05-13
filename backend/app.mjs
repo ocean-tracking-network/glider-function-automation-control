07c818231e43691e79357d73ffe39b2a4e77419a
Jesse Crocker <w0291240@nscc.ca>
2026-05-13 11:26:22 -0300

    removed dupe import for db
    removed dupe import for db

--- backend/app.mjs
+++ backend/app.mjs
@@ -35,8 +35,6 @@ import { get_logs, post_logs } from './views/logs.mjs'
 import db from './db/conn.mjs'
 import { send_slack_message } from './utils/slack.mjs'
 
-import db from './db/conn.mjs'
-
 import {
   delete_old_tracks,
   update_glider_positions,
