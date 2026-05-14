750a075b92f4026ae2576fb58f0949d19e24c703
Shyla Bennett <shyaelita@gmail.com>
2026-05-14 09:19:12 -0300

    Frontend Backend for SMS notification sign up
    Wip. Branch for connect frontend and backend.
    
    Co-Authored-By: Jesse Crocker <w0291240@nscc.ca>

--- backend/app.mjs
+++ backend/app.mjs
@@ -43,6 +43,7 @@ import {
 import './loadEnvironment.mjs'
 import { gliders_sse } from './views/sse/gliders.mjs'
 import { update_boats } from './utils/boat_utils.mjs'
+import { delete_contacts, get_contacts, create_contact, edit_contact } from './views/notify.mjs'
 
 const app = express()
 app.use(cors())
@@ -96,6 +97,14 @@ app.post('/logs', authenticateToken, requireAdmin, post_logs)
 // sse
 app.get('/sse', authenticateToken, gliders_sse.listen)
 
+//sms
+app.get('/notify', authenticateToken, get_contacts)
+app.post('/notify', authenticateToken, create_contact)
+app.patch('/notify/:id', authenticateToken, edit_contact)
+app.delete('/notify/:id', authenticateToken, delete_contacts)
+
+
+
 // boats
 // app.get('/boats/:end_offset', authenticateToken, get_boats)
 // app.get('/boats/predict/:id/:start_offset/:end_offset/:interval', get_boat_predict) //NOT USED BY THE FRONTEND ATM
@@ -106,7 +115,6 @@ const scheduleSecs = Number.parseInt(process.env.SCHEDULE_SECS, 10) || 30
 const backend_schedule = scheduleJob(`*/${scheduleSecs} * * * * *`, async () => {
   await update_glider_positions()
   await update_geofences()
-
   await delete_old_tracks()
   // update_boats()
 })
